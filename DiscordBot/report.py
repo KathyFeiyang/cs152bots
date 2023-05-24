6d165c0e3610ee2f1a0e200b8c20bda7f93499e9
daniel wan rosli <danwr@stanford.edu>
2023-05-24 12:03:32 -0700

    block and report complete

--- DiscordBot/report.py
+++ DiscordBot/report.py
@@ -8,6 +8,7 @@ class State(Enum):
     MESSAGE_IDENTIFIED = auto()
     REPORT_COMPLETE = auto()
     CHOOSE_CATEGORY = auto()
+    BLOCK_STATE = auto()
 
 class Report:
     START_KEYWORD = "report"
@@ -94,15 +95,65 @@ class Report:
             embed.set_footer(text="Please type the number corresponding to the category.")
             self.state = State.CHOOSE_CATEGORY
 
-            return [{"content":reply, "embed":embed}]
-        
-        # if self.state == State.CHOOSE_CATEGORY:
-        #     if message.content not in ABUSE_TYPES:
-        
+            return [{"content": reply, "embed": embed}]
 
-    def report_complete(self):
-        return self.state == State.REPORT_COMPLETE
-    
+        if self.state == State.BLOCK_STATE:
+
+            embed = discord.Embed(
+                color=discord.Colour.dark_blue(),
+                title="Would you like to block the content or user?"
+            )
+
+            embed.add_field(
+                name="(1)",
+                value="Block user",
+                inline=False
+            )
+
+            embed.add_field(
+                name="(2)",
+                value="Block content",
+                inline=False
+            )
+
+            embed.add_field(
+                name="(3)",
+                value="Block user and content",
+                inline=False
+            )
+
+            embed.add_field(
+                name="(4)",
+                value="Do not block",
+                inline=False
+            )
+
+            embed.set_footer(text="Please type the number corresponding to your choice.")
+
+            reply = None
+            if message.content in self.ABUSE_TYPES:
+                self.state = State.REPORT_COMPLETE
+                if message.content == '1':
+                    reply = "The user was successfully blocked."
+                elif message.content == '2':
+                    reply = "The content was successfully blocked."
+                elif message.content == '3':
+                    reply = "The user and content were successfully blocked"
+                else:
+                    reply = "Neither the user nor content was blocked."
+            else:  # In case the input is not within the ABUSE_TYPES
+                reply = "Please choose a valid option by typing the corresponding number (1, 2, 3, or 4)."
+
+            return [{"content": reply, "embed": embed}]
+
+        def report_complete(self):
+            if self.state == State.REPORT_COMPLETE:
+                return "We've received your report! " \
+                       "Thank you for taking the time to help keep our community safe and truthful." \
+                       " Our content moderation team will promptly review the report"
+            else:
+                return "Your report is still in progress. Please continue to provide the necessary information."
+
 
 
     
